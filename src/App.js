12377c281eb080051492d9a76d62dd38608908e7
cyborja <cyborj88@gmail.com>
2024-04-10 21:25:45 +0800

    add header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home.jsx';
+import Gold from './pages/Gold';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
+        <Header/>
         <Routes>
-          <Route path="/home" element={<Home/>} />
+          <Route path="/gold" element={<Gold/>} />
         </Routes>
       </Router>
     </>
---
+++ src/pages/Gold.jsx
@@ -0,0 +1,6 @@
+export default function Gold() {
+  return (
+  <>
+    <div>Gold</div>
+  </>)
+}
