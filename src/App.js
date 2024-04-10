ab9b9f1b15fd2cfe23f22648b0ad1b3731b2582d
cyborja <cyborj88@gmail.com>
2024-04-10 20:19:30 +0800

    add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
 
 function App() {
   return (
     <>
-    <h1>React is Fun!</h1>
+      <Router>
+        <Routes>
+          <Route path="/home" element={<Home/>} />
+        </Routes>
+      </Router>
     </>
   );
 }
